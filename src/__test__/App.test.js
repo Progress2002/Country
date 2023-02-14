c5176b85c8b17c3732a2e4514d724c5246b6d13a
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-02-14 13:45:14 +0100

    Update test file

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,16 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/configurStor';
-import App from '../../App';
-import HomePage from '../HomePage';
-import DetailsPage from '../DetailsPage';
-import HomepageHeader from '../HomePageHeader';
-import DetailsPageHeader from '../DetailsPageHeader';
+import store from '../redux/configurStor';
 
-jest.mock('../HomePage');
-jest.mock('../DetailsPage');
-jest.mock('../DetailsPageHeader');
+import App from '../App';
+import HomePage from '../Components/HomePage';
+import DetailsPage from '../Components/DetailsPage';
+import HomepageHeader from '../Components/HomePageHeader';
+import DetailsPageHeader from '../Components/DetailsPageHeader';
+
+jest.mock('../Components/HomePage');
+jest.mock('../Components/DetailsPage');
+jest.mock('../Components/DetailsPageHeader');
 
 describe('Test app.js with pages', () => {
   test('should render the Home page header', () => {
@@ -22,6 +23,7 @@ describe('Test app.js with pages', () => {
     const txt = screen.getByText('Countries Of The World', { exact: false });
     expect(txt).toBeInTheDocument();
   });
+
   test('should render the Details page header', () => {
     DetailsPageHeader.mockImplementation(() => <h1>Hello From Details Page Header</h1>);
     render(
@@ -32,6 +34,7 @@ describe('Test app.js with pages', () => {
     const txt = screen.getByText('Hello From Details Page Header');
     expect(txt).toBeInTheDocument();
   });
+
   test('should render the homepage', () => {
     HomePage.mockImplementation(() => <h1>Hello From Home Page</h1>);
     render(
@@ -44,10 +47,11 @@ describe('Test app.js with pages', () => {
     const txt = screen.getByText('Hello From Home Page');
     expect(txt).toBeInTheDocument();
   });
+
   test('should render the Details Page', () => {
     DetailsPage.mockImplementation(() => <h1>Hello From Details Page</h1>);
     render(
-      <MemoryRouter initialEntries={['/details/Anguilla']}>
+      <MemoryRouter initialEntries={['/details/Bangladesh']}>
         <Provider store={store}>
           <App />
         </Provider>
